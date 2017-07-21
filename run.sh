543304142ebc9528135deb3dd382539e62b58e27
joolz <joolz@xs4all.nl>
2017-07-21 10:42:54 +0200

    first version

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PROJECT=makelocalliferay
+
+echo "Make the project if it doesn't exist"
+
+test -d $PROJECT || \
+	mvn archetype:generate \
+		-DarchetypeGroupId=org.codehaus.mojo.archetypes \
+		-DarchetypeArtifactId=pom-root \
+		-DarchetypeVersion=RELEASE \
+		-DgroupId=nl.ou.dlwo \
+		-DartifactId=makelocalliferay \
+		-Dversion=1.0-SNAPSHOT \
+		-Dpackage=nl.ou.dlwo \
+		-DinteractiveMode=false
+
+pushd $PROJECT
+
+mvn clean compile -Dticketnumber=YL-1234
+
+popd
